6102a189fa153d1f8bd30b5af6a2ce4cfed5d4cd
OrestPalii <paliy.o.2004@gmail.com>
2022-03-28 22:01:57 +0300

    ready_sender

--- app/src/main/java/com/quaice/lendory/MainActivity.java
+++ app/src/main/java/com/quaice/lendory/MainActivity.java
@@ -4,6 +4,7 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,6 +16,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -30,7 +33,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button sender;
+    RelativeLayout new_adv;
+    CardView cancel, send;
     Context context;
+    EditText name_edit, desc_edit;
     ArrayList<Adv> downloaded;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +46,33 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://lendory-b5d8b-default-rtdb.firebaseio.com/");
         DatabaseReference myRef = database.getReference("advertisement");
 
+        send = findViewById(R.id.send_button);
+        name_edit = findViewById(R.id.name_edit);
+        desc_edit = findViewById(R.id.description_edit);
+        cancel = findViewById(R.id.cancel);
+        new_adv = findViewById(R.id.new_adv);
         sender = findViewById(R.id.sender);
         sender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                new_adv.setVisibility(View.VISIBLE);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new_adv.setVisibility(View.INVISIBLE);
+            }
+        });
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 ArrayList<String> arrayList = new ArrayList<>();
                 arrayList.add("dsfsafd");arrayList.add("dsfsafd1");arrayList.add("dsfsafd2");
-                Adv cur = new Adv("Оголошення","Опис","Львів","$",1000,50,2,10,
+                Adv cur = new Adv(name_edit.getText().toString(),desc_edit.getText().toString(),"Львів","$",1000,50,2,10,
                         false,arrayList,new User("Антон", "+380976547419"));
                 myRef.child("" + cur.hashCode()).setValue(cur);
-
+                new_adv.setVisibility(View.INVISIBLE);
             }
         });
 
@@ -71,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
     }
     void build_recycler(ArrayList<Adv> list){
         RecyclerView recyclerView = findViewById(R.id.recycler);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
         MyRecyclerViewAdapter  adapter = new MyRecyclerViewAdapter(this, list);
         recyclerView.setAdapter(adapter);
     }
