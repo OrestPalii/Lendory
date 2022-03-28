dcf2b9710bfbf87eda9823ed29933d0bd0fb39cf
OrestPalii <paliy.o.2004@gmail.com>
2022-03-28 12:21:07 +0300

    recycler

--- app/src/main/java/com/quaice/lendory/MainActivity.java
+++ app/src/main/java/com/quaice/lendory/MainActivity.java
@@ -4,10 +4,15 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -30,12 +35,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 FirebaseDatabase database = FirebaseDatabase.getInstance("https://lendory-b5d8b-default-rtdb.firebaseio.com/");
-                DatabaseReference myRef = database.getReference("message");
-                Adv cur = new Adv("house", "first try");
+                DatabaseReference myRef = database.getReference("message12");
+                Adv cur = new Adv("Maksim", "first try");
                 myRef.setValue(cur);
 
             }
         });
+
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        MyRecyclerViewAdapter  adapter = new MyRecyclerViewAdapter(this, data);
+        //adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
     }
 }
